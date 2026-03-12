627a26800cdcb03a5ec6a11808a741e8432ac684
NguyenXuanDung <nguyenxuand977@gmail.com>
2026-03-12 20:41:04 +0700

    api signOut ,  xong authentication

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -105,58 +105,21 @@ export const signIn = async (req, res) => {
   }
 };
 
-// export const signIn = async (req, res) => {
-//   try {
-//     //lấy inputs
-//     const { username, password } = req.body;
+export const signOut = async (req, res) => {
+  try {
+    //lấy refresh token từ cookie
+    const token = req.cookies?.refreshToken;
 
-//     if (!username || !password) {
-//       return res.status(400).json({ message: "thiếu username hoặc password" });
-//     }
-//     //lấy hashPassword trong db để so với password input
-//     const user = await User.findOne({ username });
+    if (token) {
+      //xóa refresh token trong Session
+      await Session.deleteOn({ refreshToken });
+      //xóa cookie
+      res.clearCookie("refreshToken");
+    }
 
-//     if (!user) {
-//       return res
-//         .status(401)
-//         .json({ message: "username hoặc password không chính xác" });
-//     }
-//     //kiểm tra password
-//     const passwordCorrect = await bcrypt.compare(password, user.hashedPassword);
-
-//     if (!passwordCorrect) {
-//       return res
-//         .status(401)
-//         .json({ message: "username hoặc password không chính xác" });
-//     }
-//     //nếu khớp , tạo acessToken về trong cookie
-//     const accessToken = jwt.sign(
-//       { userId: user._id },
-//       process.env.ACCESS_TOKEN_SECRET,
-//       { expiresIn: ACCESS_TOKEN_TTL },
-//     );
-//     //tạo refresh token
-//     const refreshToken = crypto.randomBytes(64).toString("hex");
-
-//     //tạo session mới để lưu refresh token
-//     await Session.create({
-//       userId: user._id,
-//       refreshToken,
-//       expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL),
-//     });
-//     //trả refresh token về trong cookie
-//     res.cookie("refreshToken", refreshToken, {
-//       httpOnly: true,
-//       secure: true,
-//       sameSite: "none", //backen , frontend deploy riêng
-//       maxAge: REFRESH_TOKEN_TTL,
-//     });
-//     //trả access token trong res
-//     return res
-//       .status(200)
-//       .json({ message: `User ${user.displayName} đã logged in!`, accessToken });
-//   } catch (error) {
-//     console.error("Lỗi khi gọi signIn", error);
-//     return res.status(500).json({ message: "Lỗi hệ thống" });
-//   }
-// };
+    return res.sendStatus(204);
+  } catch (error) {
+    console.error("Lỗi khi gọi signOut", error);
+    return res.status(500).json({ message: "Lỗi hệ thống" });
+  }
+};
