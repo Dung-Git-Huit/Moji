e877c9934ba08160a47e36283b11cf88d6594bc5
NguyenXuanDung <nguyenxuand977@gmail.com>
2026-03-12 18:37:45 +0700

    api signup

---
+++ backend/src/models/User.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const userSChema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    hashedPassword: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    displayName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    avartarUrl: {
+      type: String, //link CDN để hiển thị hình
+    },
+    avatarId: {
+      type: String, //cloudinary public_id để xóa ảnh
+    },
+    bio: {
+      type: String,
+      maxlength: 500,
+    },
+    phone: {
+      type: String,
+      sparse: true, //cho phép null,nhưng ko được trùng lặp
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("User", userSChema);
+
+export default User;
