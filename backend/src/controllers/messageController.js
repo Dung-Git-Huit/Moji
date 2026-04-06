816216f233ec3c34939b16af11da44774030552a
NguyenXuanDung <nguyenxuand977@gmail.com>
2026-04-06 16:14:42 +0700

    message api

---
+++ backend/src/controllers/messageController.js
@@ -0,0 +1,49 @@
+import Conversation from "../models/Conversation.js";
+import Message from "../models/Message.js";
+import { updateConversationAfterCreateMessage } from "../utils/messageHelps.js";
+export const sendDirectMessage = async (req, res) => {
+  try {
+    const { recipientId, content, conversationId } = req.body;
+    const senderId = req.user._id;
+
+    let conversation;
+
+    if (!content) {
+      return res.status(400).json({
+        message: "Nội dung tin nhắn không được để trống",
+      });
+    }
+
+    if (conversationId) {
+      conversation = await Conversation.findById(conversationId);
+    }
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        type: "direct",
+        participants: [
+          { userId: senderId, joinedAt: new Date() },
+          { userId: recipientId, joinedAt: new Date() },
+        ],
+        lastMessageAt: new Date(),
+        unreadCounts: new Map(),
+      });
+    }
+
+    const message = await Message.create({
+      conversationId: conversation._id,
+      senderId,
+      content,
+    });
+    updateConversationAfterCreateMessage(conversation, message, senderId);
+
+    await conversation.save();
+
+    return res.status(201).json({ message });
+  } catch (error) {
+    console.log("Lỗi xảy ra khi gửi tin nhắn trực tiếp", error);
+    res.status(500).json({ message: "Lỗi hệ thống", error });
+  }
+};
+
+export const sendGroupMessage = async (req, res) => {};
