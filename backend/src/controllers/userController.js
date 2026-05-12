ab258ebfa6215c55fb64d0fd8ac862e4c11f5707
NguyenXuanDung <nguyenxuand977@gmail.com>
2026-05-12 21:17:44 +0700

    api searchUserByUsername

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,3 +1,4 @@
+import User from "../models/User.js";
 export const authMe = async (req, res) => {
   try {
     const user = req.user; //lấy từ authMiddleware
@@ -8,6 +9,23 @@ export const authMe = async (req, res) => {
   }
 };
 
-export const test = async (req, res) => {
-  return res.sendStatus(204);
+export const searchUserByUsername = async (req, res) => {
+  try {
+    const { username } = req.query;
+
+    if (!username || username.trim() === "") {
+      return res
+        .status(400)
+        .json({ message: "Cần cung cấp user trong query." });
+    }
+
+    const user = await User.findOne({ username }).select(
+      "_id displayName username avatar",
+    );
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.error("Lỗi xảy ra khi searchUserByUsername");
+    return res.status(500).json({ message: "Lỗi hệ thống" });
+  }
 };
